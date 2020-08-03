f6885c1cad8839f0275c3865b206321b320ad608
elyabe <elyabe.as@gmail.com>
2020-08-03 18:23:56 -0300

    Basic structure

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+
+      <button type="button">
+        Clique em mim
+      </button>
+    </div>
+  );
+}
+
+export default App;
